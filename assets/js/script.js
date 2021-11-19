a021ad45e7e55d0bd4c7dfe9fa20edcd6781c15f
Sebabin <sebastiancrespin279@gmail.com>
2021-11-19 14:04:04 -0700

    now have drag and drop capabilities for changing status as well as deleting items

--- assets/js/script.js
+++ assets/js/script.js
@@ -136,6 +136,64 @@ $(".list-group").on("blur", "input[type='text']", function() {
          $(this).replaceWith(taskSpan);
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  srcoll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    //console.log("activate", this);
+  },
+  deactivate: function(event) {
+    //console.log("deactivate", this);
+  },
+  over: function(event) {
+   // console.log("out", event.target);
+  },
+  update: function() {
+    var tempArr = [];
+   // loop over current set of children in sortable  list
+   $(this).children().each(function() {
+     
+     var text = $(this)
+     .find("p")
+     .text()
+     .trim();
+     var date = $(this)
+       .find("span")
+       .text()
+       .trim();
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+   });
+  // console.log(tempArr);
+   // trim down lists ID to match object property
+   var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+      // update array on tasks object and save
+      tasks[arrName] = tempArr;
+      saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event,ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+     console.log("out");
+  }
+});
 
 
 // modal was triggered
