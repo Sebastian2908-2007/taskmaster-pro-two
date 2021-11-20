242134aac62f83c895eef69c86ba8164c5016583
Sebabin <sebastiancrespin279@gmail.com>
2021-11-19 17:18:23 -0700

    app now warns users with color if task is late or drawing near

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
-
+// check due date
+auditTask(taskLi);
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -101,12 +102,21 @@ $(".list-group").on("click", "span", function() {
       // swap out elements
       $(this).replaceWith(dateInput);
 
+      // enable jquery ui datepicker
+      dateInput.datepicker({
+        minDate: 1,
+        onClose: function() {
+          // when caleender is closed , force a "change" event on the dateInput
+          $(this).trigger("change");
+        }
+      });
+
       // automatically focus on new element
       dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get curerrnt text
   var date = $(this)
       .val()
@@ -134,6 +144,9 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
          // replace input with span element
          $(this).replaceWith(taskSpan);
+
+         // pass tasks <li> element into auditTask() to check the new due date
+         auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 $(".card .list-group").sortable({
@@ -194,6 +207,29 @@ $("#trash").droppable({
      console.log("out");
   }
 });
+
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+
+var auditTask = function(taskEl) {
+  var date = $(taskEl).find("span").text().trim();
+  // ti ensure task is getting to the function
+
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  // removwe any old  classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near or over due date
+  if(moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }else if(Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+ 
+};
 
 
 // modal was triggered
