f7b8d12485051570f83ed1723aac928caabffcf5
Sebabin <sebastiancrespin279@gmail.com>
2021-11-19 18:02:11 -0700

    done for day commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -228,7 +228,7 @@ var auditTask = function(taskEl) {
   }else if(Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
- 
+ console.log(taskEl);
 };
 
 
@@ -279,3 +279,8 @@ $("#remove-tasks").on("click", function() {
 loadTasks();
 
 
+setInterval(function() {
+ $(".card .list-group-item").each(function(index,el) {
+   auditTask(el);
+ });
+},1800000);
