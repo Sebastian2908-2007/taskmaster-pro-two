6a7870537c33b4fa17340585e8472a6cdc5a6e8c
Sebabin <sebastiancrespin279@gmail.com>
2021-11-20 17:03:44 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -155,13 +155,23 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    //console.log("activate", this);
+    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-    //console.log("deactivate", this);
+    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
    // console.log("out", event.target);
+  
+   $(this).addClass("dropover-active");
+  },
+  out: function(event) {
+ 
+  $(this).removeClass("dropover-active");
   },
   update: function() {
     var tempArr = [];
@@ -199,12 +209,15 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event,ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
      console.log("out");
+     $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -245,7 +258,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
